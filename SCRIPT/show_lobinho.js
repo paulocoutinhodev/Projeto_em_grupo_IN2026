996ee4cddfb21bbd26c9bfb1bd574975b9f0cf59
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-07 10:30:46 -0300

    feat: implementando botão de excluir

--- SCRIPT/show_lobinho.js
+++ SCRIPT/show_lobinho.js
@@ -1,4 +1,5 @@
 import { buscarLoboPorId } from "../API/api_get_lista_de_lobinhos.js";
+import { excluirLobo } from "../API/api_delete_show_lobinho.js";
 
 const imgLobo = document.querySelector('.wolf-image img')
 const nomeLoboTexto = document.querySelector('.title')
@@ -21,13 +22,28 @@ async function carregarLobo() {
             idLoboTexto.textContent = `${lobo.descricao}`
             imgLobo.src = lobo.imagem;
             imgLobo.alt = `Foto do lobo${lobo.nome}`;
+            
             const linkDestino = `adotar_lobinho.html?id=${lobo.id}`;
-            adoptionDiv.innerHTML += `
+            const adoptButton = `
              <a href = "${linkDestino}" id="destiny">
                     <button class = "btn-adopt">Adotar</button>
             </a>`;
+            const deleteButton = `<button id="btn-excluir" class="btn-delete">Excluir</button>`;
+
+            adoptionDiv.innerHTML = adoptButton + deleteButton;
+
+            const btnDeletWolf = document.getElementById('btn-excluir');
+            btnDeletWolf.addEventListener('click', async () => {
+                try {
+                    await excluirLobo(id);
+                    window.location.href="lista_de_lobinhos.html";
+                    alert("Lobo excluído com sucesso!");
+
+                } catch (error) {
+                    alert("Erro ao excluir")
+                }                
+            });
         }
-            
     } catch (error) {       
         console.error("Erro ao carregar lobo:", error);
         nomeLoboTexto.textContent = "Erro ao carregar informações.";
